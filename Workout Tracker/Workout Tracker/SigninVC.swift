6cf86a88636836033000e6bca8c0be862cb8986f
Corey <seschwan@yahoo.com>
2020-02-15 09:15:14 -0700

    SignIn storyboard layout, Added Signin VC
    Laid out the signin storyboard components. Also added the signin VC and connected the outlets.

---
+++ Workout Tracker/Workout Tracker/SigninVC.swift
@@ -0,0 +1,46 @@
+//
+//  SigninVC.swift
+//  Workout Tracker
+//
+//  Created by Seschwan on 2/15/20.
+//  Copyright © 2020 LambdaLabsPT7. All rights reserved.
+//
+
+import UIKit
+
+class SigninVC: UIViewController {
+    
+    // MARK: - Outlets
+    // UITextFields
+    @IBOutlet weak var emailTextField:    UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    // UIButtons
+    @IBOutlet weak var forgotPasswordButton: UIButton!
+    @IBOutlet weak var signInButton:         UIButton!
+    @IBOutlet weak var newSignupButton:      UIButton!
+    
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+    
+    // MARK: - Actions
+    @IBAction func forgotPasswordBtnPressed(_ sender: UIButton) {
+        
+    }
+    
+    @IBAction func signInBtnPressed(_ sender: UIButton) {
+        
+    }
+    
+    @IBAction func newSignupBtnPressed(_ sender: UIButton) {
+        
+    }
+    
+
+}
