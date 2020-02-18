764164c421e23d03a39363f8c96e5eab3585c24f
Stephanie <sbowl001@fiu.edu>
2020-02-18 12:58:09 +0800

    added auto layouts to onboarding

--- Workout Tracker/Workout Tracker/OnboardingFirstViewController.swift
+++ Workout Tracker/Workout Tracker/OnboardingFirstViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class OnboardingFirstViewController: UIViewController {
 
+    @IBOutlet weak var multiplechoice1: SmallButtonExtension!
+    @IBOutlet weak var multiplechoice4: SmallButtonExtension!
     @IBOutlet weak var backgroundView: UIView!
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -23,4 +25,27 @@ class OnboardingFirstViewController: UIViewController {
 
     
 
+}
+
+
+class SmallButtonExtension: UIButton {
+ 
+
+    override func point(inside point: CGPoint, with event: UIEvent?) -> Bool {
+        let newArea = CGRect(
+            x: self.bounds.origin.x - 5.0,
+            y: self.bounds.origin.y - 5.0,
+            width: self.bounds.size.width + 10.0,
+            height: self.bounds.size.height + 20.0
+        )
+        return newArea.contains(point)
+    }
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
 }
