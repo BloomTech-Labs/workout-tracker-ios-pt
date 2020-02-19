8e4a303ccdbcdfb2afc118c80d944fc63b329f3a
Corey <seschwan@yahoo.com>
2020-02-18 20:36:31 -0700

    Added the Schedule VC and layout.
    Laid out some of the Schedule storyboard.

---
+++ Workout Tracker/Workout Tracker/CreateANewScheduleVC.swift
@@ -0,0 +1,30 @@
+//
+//  CreateANewScheduleVC.swift
+//  Workout Tracker
+//
+//  Created by Seschwan on 2/18/20.
+//  Copyright © 2020 LambdaLabsPT7. All rights reserved.
+//
+
+import UIKit
+
+class CreateANewScheduleVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
