aae28f5b4b72c7a17cb220e271367d01aba8c701
Corey <seschwan@yahoo.com>
2020-02-19 20:47:35 -0700

    Laid out CreateASchedule VC and added the Outlets/Actions
    Added the items to the VC and added the outlets and actions in the corresponding VC.

--- Workout Tracker/Workout Tracker/CreateANewScheduleVC.swift
+++ Workout Tracker/Workout Tracker/CreateANewScheduleVC.swift
@@ -9,22 +9,41 @@
 import UIKit
 
 class CreateANewScheduleVC: UIViewController {
-
+    
+    // MARK: - Outlets
+    
+    @IBOutlet weak var nameYourWorkoutTextField: UITextField!
+    @IBOutlet weak var dateTextField:            UITextField!
+    @IBOutlet weak var timeTextField:            UITextField!
+    @IBOutlet weak var durationTextField:        UITextField!
+    @IBOutlet weak var affirmationTextField:     UITextField!
+    
+    @IBOutlet weak var reminderToggle: UISwitch!
+    
+    @IBOutlet weak var libraryButton: UIButton!
+    @IBOutlet weak var saveButton:    UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    
+    // MARK: - Actions
+    
+    
+    @IBAction func libraryBtnPressed(_ sender: UIButton) {
+        
     }
-    */
-
+    
+    @IBAction func reminderToggleSwitched(_ sender: UISwitch) {
+        
+    }
+    
+    @IBAction func saveBtnPressed(_ sender: UIButton) {
+        dismiss(animated: true, completion: nil)
+    }
+    
 }
