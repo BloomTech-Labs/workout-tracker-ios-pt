8dd5db85f60b980f0abc40b1814a652251d79b24
Stephanie <sbowl001@fiu.edu>
2020-02-25 13:03:06 +0800

    Added calendar support files

---
+++ Workout Tracker/Workout Tracker/CalendarMainViewController.swift
@@ -0,0 +1,30 @@
+//
+//  CalendarMainViewController.swift
+//  Workout Tracker
+//
+//  Created by Stephanie Bowles on 2/25/20.
+//  Copyright © 2020 LambdaLabsPT7. All rights reserved.
+//
+
+import UIKit
+
+class CalendarMainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Workout Tracker/Workout Tracker/DateHeader.swift
@@ -0,0 +1,9 @@
+//
+//  DateHeader.swift
+//  Workout Tracker
+//
+//  Created by Stephanie Bowles on 2/25/20.
+//  Copyright © 2020 LambdaLabsPT7. All rights reserved.
+//
+
+import Foundation
