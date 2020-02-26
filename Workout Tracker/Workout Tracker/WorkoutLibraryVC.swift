866e73dd6a99277d7c06af69f443cced5ee796a3
Corey <seschwan@yahoo.com>
2020-02-25 22:03:14 -0700

    Added the tableView to the VC and linked up the TableViewCells

--- Workout Tracker/Workout Tracker/WorkoutLibraryVC.swift
+++ Workout Tracker/Workout Tracker/WorkoutLibraryVC.swift
@@ -9,9 +9,15 @@
 import UIKit
 
 class WorkoutLibraryVC: UIViewController {
+    
+    // MARK: - Outlets
+    @IBOutlet weak var tableView: UITableView!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        tableView.dataSource = self
+        tableView.delegate = self
 
         // Do any additional setup after loading the view.
     }
@@ -27,4 +33,22 @@ class WorkoutLibraryVC: UIViewController {
     }
     */
 
+}
+
+extension WorkoutLibraryVC: UITableViewDataSource, UITableViewDelegate {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10 // Need to change this!
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        
+        cell.textLabel?.text = "Test"
+        
+        return cell
+    }
+    
+    
+    
 }
