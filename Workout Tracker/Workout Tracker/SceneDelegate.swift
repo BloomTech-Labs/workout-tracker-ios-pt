125c13619c9fc0d5de49b53341ca360eb74cc786
Stephanie <sbowl001@fiu.edu>
2020-03-02 12:42:11 +0800

    cleaned up some code on calendar, added detailactivityVC

--- Workout Tracker/Workout Tracker/SceneDelegate.swift
+++ Workout Tracker/Workout Tracker/SceneDelegate.swift
@@ -18,7 +18,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
         guard let _ = (scene as? UIWindowScene) else { return }
-        window?.rootViewController =  UIStoryboard(name: "Onboarding", bundle: nil).instantiateInitialViewController()
+        window?.rootViewController =  UIStoryboard(name: "MyActivities", bundle: nil).instantiateInitialViewController()
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
