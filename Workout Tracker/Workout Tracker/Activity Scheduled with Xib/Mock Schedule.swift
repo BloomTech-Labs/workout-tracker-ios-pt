4fdf8d00596086a40cdf95bc9c074024f7d163e8
Stephanie <sbowl001@fiu.edu>
2020-03-19 11:50:00 +0800

    finished schedule randomizer

--- Workout Tracker/Workout Tracker/Activity Scheduled with Xib/Mock Schedule.swift
+++ Workout Tracker/Workout Tracker/Activity Scheduled with Xib/Mock Schedule.swift
@@ -24,10 +24,31 @@ extension Schedule {
         
         let day = [Int](0...27).randomValue()
         let hour = [Int](0...23).randomValue()
+        let month = [Int](0...12).randomValue()
+        let year = 2020
         let startDate = Calendar.current.date(byAdding: .day, value: day, to: fromStartDate)!
         
         
         startTime = Calendar.current.date(byAdding: .hour, value: hour, to: startDate)!
+        
+        var dateComponent = DateComponents()
+        dateComponent.month = month
+        dateComponent.year = year
+        dateComponent.day = day
+        
+//        let dateComponents = DateComponents(year: year, day: day, month: month)
+        dateScheduled = Calendar.current.date(byAdding: dateComponent, to: startDate)!
       
+    }
+}
+extension Schedule : Equatable {
+    static func ==(lhs: Schedule, rhs: Schedule) -> Bool {
+        return lhs.startTime == rhs.startTime
+    }
+}
+
+extension Schedule : Comparable {
+    static func <(lhs: Schedule, rhs: Schedule) -> Bool {
+        return lhs.startTime < rhs.startTime
     }
 }
