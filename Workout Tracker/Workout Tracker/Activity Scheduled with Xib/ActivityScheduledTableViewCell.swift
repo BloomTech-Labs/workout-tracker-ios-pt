17314078896ac657f161724a1847505d8634a55d
Stephanie <sbowl001@fiu.edu>
2020-03-19 10:29:03 +0800

    added xib for activity scheduled

---
+++ Workout Tracker/Workout Tracker/Activity Scheduled with Xib/ActivityScheduledTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  ActivityScheduledTableViewCell.swift
+//  Workout Tracker
+//
+//  Created by Stephanie Bowles on 3/19/20.
+//  Copyright © 2020 LambdaLabsPT7. All rights reserved.
+//
+
+import UIKit
+
+class ActivityScheduledTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
