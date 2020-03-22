88f13e6993067d845d7da94e9516315825cac746
Stephanie <sbowl001@fiu.edu>
2020-03-22 18:13:10 +0800

    added base code for video color layer, not working yet

--- Workout Tracker/Workout Tracker/LandingPageViewController.swift
+++ Workout Tracker/Workout Tracker/LandingPageViewController.swift
@@ -14,21 +14,26 @@ class LandingPageViewController: UIViewController {
     @IBOutlet weak var videoBackground: UIView!
     @IBOutlet weak var getStartedButton: UIButton!
     
+    
     var player: AVPlayer?
+    let videoURL = Bundle.main.url(forResource: "workoutvid", withExtension: "mp4")!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
         getStartedButton.layer.cornerRadius = 10
         getStartedButton.layer.maskedCorners = [.layerMinXMinYCorner]
+        let playerItem = AVPlayerItem(url: videoURL)
+        playerItem.videoComposition = createVideoComposition(for: playerItem)
         playBackgroundVideo()
         
     }
     
     func playBackgroundVideo(){
-        guard let path = Bundle.main.path(forResource: "workoutvid", ofType: "mp4") else { return}
+        
        
-        player = AVPlayer(url: URL(fileURLWithPath: path))
+        player = AVPlayer(url: videoURL)
         player!.actionAtItemEnd = AVPlayer.ActionAtItemEnd.none
         let playerLayer = AVPlayerLayer(player: player)
         playerLayer.frame = self.videoBackground.frame
@@ -44,5 +49,19 @@ class LandingPageViewController: UIViewController {
     @objc func playerItemDidReachEnd(){
         player!.seek(to: CMTime.zero)
     }
-   
+    
+    func createVideoComposition(for playerItem: AVPlayerItem) -> AVVideoComposition {
+       let composition = AVVideoComposition(asset: playerItem.asset, applyingCIFiltersWithHandler: { request in
+         // Here we can use any CIFilter
+         guard let filter = CIFilter(name: "CIColorMonochrome") else {
+           return request.finish(with: NSError())
+         }
+         filter.setValue(request.sourceImage, forKey: "inputImage")
+         filter.setValue(CIColor(red: 212, green: 104, blue: 41), forKey: "inputColor")
+
+         filter.setValue(1.0, forKey: "inputIntensity")
+         return request.finish(with: filter.outputImage!, context: nil)
+       })
+       return composition
+     }
 }
