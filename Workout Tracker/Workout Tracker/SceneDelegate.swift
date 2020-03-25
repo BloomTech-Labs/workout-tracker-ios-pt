c97d33bc12a45a37ab4f89fc141aaca7710f599d
Stephanie <sbowl001@fiu.edu>
2020-03-25 11:55:45 +0800

    latest

--- Workout Tracker/Workout Tracker/SceneDelegate.swift
+++ Workout Tracker/Workout Tracker/SceneDelegate.swift
@@ -18,7 +18,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
         guard let _ = (scene as? UIWindowScene) else { return }
-        window?.rootViewController =  UIStoryboard(name: "MyActivities", bundle: nil).instantiateInitialViewController()
+//        window?.rootViewController =  UIStoryboard(name: "MyActivities", bundle: nil).instantiateInitialViewController()
         
       /*
          this would orrived scene delegate presentation , main tab bar controller of the Dashboard would be the "Main"storyboard & then could do : 
